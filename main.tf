ffc5e2b17907d586591612e9d537bcc9dd1886d0
ayobaymak <ayobaymak@gmail.com>
2022-01-04 08:25:48 +0000

    Updated pipeline file

--- main.tf
+++ main.tf
@@ -5,6 +5,16 @@ provider "azurerm"{
     }
 }
 
+terraform {
+  backend "azurerm" {
+       resource_group_name = "tf_rg_blobstore"
+       storage_account_name = "tfstorageaccountbaymak"
+       container_name = "tfstate"
+       key = "terraform.tfstate"
+  }
+ 
+}
+
 resource "azurerm_resource_group" "tf_test" {
     name = "tfmainrg"
     location = "West Europe"
